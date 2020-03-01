25f517db01014ed32d1b01eea2fa68f5d52c1e54
Ishita Singhal <32170339+ishitasinghal@users.noreply.github.com>
2020-03-01 16:18:56 +0530

    Modified!

--- artifact1/src/main/java/Selenium/group/project1/artifact1/website.java
+++ artifact1/src/main/java/Selenium/group/project1/artifact1/website.java
@@ -1,31 +1,44 @@
 package Selenium.group.project1.artifact1;
-import org.openqa.selenium.*;
+import org.testng.annotations.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 public class website {
 	
-	public static void main(String[] args) throws InterruptedException{
-		System.setProperty("webdriver.chrome.driver","C:\\Selenium Softwares\\chromedriver.exe");
-        WebDriver driver= new ChromeDriver();
-        
+	WebDriver driver;
+	
+	@BeforeTest
+	void configure(){
+		String url="https://www.trivago.in/";
+		System.setProperty("webdriver.chrome.driver", "C:\\Selenium Softwares\\chromedriver.exe");
+		driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get(url);
+	}
+	
+	@Test
+	void navigation() {
+		
         driver.navigate().to("http://www.google.com/");
         WebElement e1 = driver.findElement(By.name("q"));
 	   	e1.sendKeys("trivago");
         driver.navigate().to("https://www.trivago.in/");
+	}
+	
+	@Test
+	void searchHotel() {
         
         WebElement e2 = driver.findElement(By.id("querytext"));
         e2.sendKeys("Bengaluru");
         
         WebElement e3 = driver.findElement(By.xpath("//*[@id=\"js-fullscreen-hero\"]/div[1]/div[2]/button[2]/span[2]"));
         e3.click();
+        
+	}
+	
+	@AfterTest
+	void closecon() {
 	     driver.close();
 	  }
 	}
-
- 
-
-    
-   
-        
-        
-       
-        
