a5bd9068e29d763ba5c0e64b1033d9ba2ebf5f2f
500060722 <500060722@stu.upes.ac.in>
2020-03-14 12:12:09 +0530

    Added @DataProvider annotation.Working successfully

--- TestNGMidsemProject/src/trivago/ChromeConfigureLoginFacebook.java
+++ TestNGMidsemProject/src/trivago/ChromeConfigureLoginFacebook.java
@@ -7,11 +7,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class ChromeConfigureLoginFacebook {
 	
 	WebDriver obj;
+	
+	@DataProvider(name = "data-provider")
+    public Object[][] dataProviderMethod() {
+        return new Object[][] { { "prajjawalbanati16@gmail.com" , "u6y2IO@1" } };
+    }
 	
 	@BeforeTest
 	void configure(){
@@ -31,18 +37,18 @@ public class ChromeConfigureLoginFacebook {
 		element.click();
 	}
 	
-	@Test (priority=1)
-	void signinoption()
+	@Test (priority=1,dataProvider="data-provider")
+	void signinoption(String username,String password)
 	{
 		WebElement element,element1,element2,element3;
 		element=obj.findElement(By.xpath("//*[@id=\"unified-sign-in\"]/section/div/div/div[1]/div[2]/ul/li[3]/button/span[2]"));
 		element.click();
 		element1=obj.findElement(By.id("email"));
 		Assert.assertTrue(element1.isDisplayed());
-		element1.sendKeys("prajjawalbanati16@gmail.com");
+		element1.sendKeys(username);
 		element2=obj.findElement(By.id("pass"));
 		Assert.assertTrue(element2.isDisplayed());
-		element2.sendKeys("u6y2IO@1");
+		element2.sendKeys(password);
 		element3=obj.findElement(By.id("loginbutton"));
 		String name=element3.getText();
 		Assert.assertTrue(name.contains("Log In"));
@@ -52,7 +58,7 @@ public class ChromeConfigureLoginFacebook {
 	@AfterTest
 	void quitbrowser()
 	{
-		obj.quit();		
+		//obj.quit();		
 	}
 
 }
